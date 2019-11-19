4842f80254f95082f04f40af7238e63ba4e67b29
Philip Daniels <Philip.Daniels1971@gmail.com>
2019-11-19 15:57:53 +0000

    Can't get async/parallel to work. Rip it out!

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,6 @@ use std::io::{self, Cursor, Write};
 use std::num::ParseFloatError;
 use std::path::Path;
 use std::str::FromStr;
-use futures::stream::{FuturesUnordered, StreamExt};
-use reqwest::Client;
 
 // Original time: 5m16s.
 
@@ -138,8 +136,7 @@ fn main() {
 
     println!("Data files read successfully. Beginning download of {} prices.", stocks.len());
 
-    let mut runtime = tokio::runtime::Runtime::new().expect("Unable to create a runtime");
-    let (new_prices, errors) = runtime.block_on(download_prices(&stocks, &download_sources));
+    let (new_prices, errors) = download_prices(&stocks, &download_sources);
 
     println!("Writing output files.");
     let output_dir = env::current_dir().expect("Could not determine current directory, so cannot write any output");
@@ -160,44 +157,28 @@ fn read_csv<T: Debug + DeserializeOwned>(rdr: &mut Cursor<&[u8]>) -> std::io::Re
     Ok(records)
 }
 
-async fn download_prices(stocks: &[Stock], sources: &[Source]) -> (Vec<Price>, Vec<String>) {
+fn download_prices(stocks: &[Stock], sources: &[Source]) -> (Vec<Price>, Vec<String>) {
     let mut prices = Vec::new();
     let mut errors = Vec::new();
-
-    let client = Client::new();
-    let mut futures = FuturesUnordered::new();
 
     for stock in stocks {
         let source = sources.iter().find(|s| s.id == stock.source_id)
             .expect(&format!("Cannot find Source for Stock {}", stock.symbol));
 
-        futures.push(download_price(stock, source, &client));
-    }
-
-    while let Some(res) = futures.next().await {
-        match res {
+        match download_price(&stock, &source) {
             Ok(price) => prices.push(price),
-            Err(e) => {
-                         errors.push(format!("Could not download price, error is {}", e))
-                     }
+            Err(e) => errors.push(format!("Could not download price, error is {}", e)),
         }
     }
 
     (prices, errors)
 }
 
-async fn download_price(stock: &Stock, source: &Source, client: &Client) -> Result<Price, StockPriceError> {
+fn download_price(stock: &Stock, source: &Source) -> Result<Price, StockPriceError> {
     let url = format!("{}{}", source.url, stock.digital_look_name);
     println!("Downloading {} from {}", stock.symbol, url);
-    let url = reqwest::Url::parse(&url).unwrap();
 
-    // let mut body = client.get(url)
-    //     .await?
-    //     .text()
-    //     .await?;
-
-    let mut body = reqwest::get(url).await?.text().await?;
-    //let mut body = reqwest::get(&url)?.text()?;
+    let mut body = reqwest::blocking::get(&url)?.text()?;
     println!("  Document downloaded.");
 
     // Utc::today is Ok unless we run past midnight, which is not a concern for this program.
